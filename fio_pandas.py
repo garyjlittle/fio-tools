bb37b7a1f2fbc1e58458a11374e4fa0e0219753b
Gary Little <gary@noderunner.com>
2023-05-22 14:08:55 -0400

    Working version

--- fio_pandas.py
+++ fio_pandas.py
@@ -11,27 +11,41 @@ import argparse
 def main():
     #pddict={}
     pdsum=pd.DataFrame()
+    
 
     #PNG output filename
     imgname='pandas-pandas.png'
     parser=argparse.ArgumentParser()
-    parser.add_argument("-a","--anything",action="store")
     parser.add_argument("-m","--metric",action="store",dest="metric",help="Type of metric")
-    args,extras=parser.parse_known_args()
-    print("args are ",args)
-    print("metric is ",args.metric)
+    parser.add_argument("--fields",action="store",dest="field_count",help="Format of the fio file (number of fields)",type=int,default=4)
+    parser.add_argument("-s","--summary",action="store_true",help="Sum up the log files and show the aggregate value")
+    parser.add_argument("-d","--debug",action="store_true",help="Debug")
+    parser.add_argument("--table",action="store_true",help="show summary text table")
+    args,filenames=parser.parse_known_args()
     metric=args.metric
-    print("switch is ",args.anything)
-    arglen=len(extras)
+    debug=args.debug
+    field_count=args.field_count
+    arglen=len(filenames)
     i=0
-    for filename in extras:
-        # pandas seems to tread "-" as a separator, so replace with "_" for now
+    for filename in filenames:
+        # pandas seems to treat "-" as a separator, so replace with "_" for now so that the dataframe
+        # is named after the file.  We create one df per file.
         pdname=filename.replace("-","_")
-        #we dont need the full dict for the simple case - leave it here for reference
-        #pddict[pdname]=pd.read_csv(filename,names=["time","value","direction","priority"])
-        pdtmp=pd.read_csv(filename,names=["time","value","direction","priority"])
-        #i-1 because i starts at 1.  Divide by 1000 to get MB/s
+        # read in the file into a temporary dataframe.  This format has changed over fime
+        # specify this at the CLI for now, but try to figure it out dynamically
+        if field_count==4:
+            pdtmp=pd.read_csv(filename,names=["time","value","direction","priority"])
+        if field_count==5:
+            #Not sure if/what's happening here the format does not match the docs. 
+            pdtmp=pd.read_csv(filename,names=["time","value","direction","priority","dummy1"])        
+        if debug:
+            print("filename is ",filename)
+            print(pdtmp)
+            exit()
+        # pdsum is the name of the df that we use to collect data for each file even
+        # if we don't actually sum up the results.
         if metric == "bandwidth":
+            # fio reports in kb/s but probably we want to show MB/s
             pdsum.insert(0,pdname,pdtmp["value"]/1000)
             metric_name="Bandwidth (MB/s)"
         else:
@@ -40,8 +54,12 @@ def main():
     
         i+=1
 
-    pdsum=pdsum.assign(total=pdsum.sum(axis=1))
-    print(pdsum)
+    if (args.summary):
+        # Here we sum the entry for each file and create an aggregate/sum
+        # if we ask for it on the command line.
+        pdsum=pdsum.assign(total=pdsum.sum(axis=1))
+    if (args.table):
+        print(pdsum)
 
     ax=pdsum.plot()
     ax.set_xlabel("Time")
@@ -49,7 +67,7 @@ def main():
     ax.figure.savefig(imgname)
 
     #Special case for iterm2 imgcat
-    subprocess.call(['/bin/bash', '-i', '-c','-l', 'imgcat '+imgname])
+    subprocess.call(['/bin/bash','-i', '-c','-l', 'imgcat '+imgname])
 
 
 
---
+++ testimgcat.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.call(['/bin/bash', '-i', '-c','-l', 'imgcat '+'pandas-pandas.png'])
