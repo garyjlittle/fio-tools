0edd6b34de6408fc3a9799069ad0cc13d4ea4369
Gary Little <gary@noderunner.com>
2023-05-23 16:37:39 -0400

    Improve plotting

--- fio_pandas.py
+++ fio_pandas.py
@@ -14,33 +14,32 @@ def main():
     pdsum_write=pd.DataFrame()
     pdsum=pd.DataFrame()
 
-    
-
-    #PNG output filename
     imgname='pandas-pandas.png'
     parser=argparse.ArgumentParser()
     parser.add_argument("-m","--metric",action="store",dest="metric",help="Type of metric")
-    parser.add_argument("--fields",action="store",dest="field_count",help="Format of the fio file (number of fields)",type=int,default=4)
+    parser.add_argument("--dpi",action="store",help="DPI of output",type=int)
     parser.add_argument("-s","--summary",action="store_true",help="Sum up the log files and show the aggregate value")
     parser.add_argument("-d","--debug",action="store_true",help="Debug")
     parser.add_argument("--table",action="store_true",help="show summary text table")
     args,filenames=parser.parse_known_args()
     metric=args.metric
     debug=args.debug
-    field_count=args.field_count
+    dpi=args.dpi
     arglen=len(filenames)
-    i=0
     for filename in filenames:
         # pandas seems to treat "-" as a separator, so replace with "_" for now so that the dataframe
         # is named after the file.  We create one df per file.
         pdname=filename.replace("-","_")
         # read in the file into a temporary dataframe.  This format has changed over fime
         # specify this at the CLI for now, but try to figure it out dynamically
+        pdtmp=pd.read_csv(filename)
+        # Grab the column count
+        field_count=pdtmp.shape[1]
         if field_count==4:
-            pdtmp=pd.read_csv(filename,names=["time","value","direction","priority"])
+            pdtmp.columns=["time","value","direction","priority"]
         if field_count==5:
             #Not sure if/what's happening here the format does not match the docs. 
-            pdtmp=pd.read_csv(filename,names=["time","value","direction","priority","dummy1"])        
+            pdtmp.columns=["time","value","direction","priority","dummy1"]       
         if debug:
             print("filename is ",filename)
             print(pdtmp)
@@ -70,7 +69,6 @@ def main():
         pdsum_read.insert(0,pdname,pdtmp_read["value"])
         pdsum_write.insert(0,pdname,pdtmp_write["value"])
         metric_name="IOPS"
-        i+=1
 
     # By here pdsum, pdsum_read and pdsum_write have been created
     pdsum_read=pdsum_read.assign(read_total=pdsum_read.sum(axis=1))    
@@ -100,28 +98,43 @@ def main():
     #  for read only test
     ax=pdsum_read.plot()
     metric_name="read IOPS"
+    ax.legend(bbox_to_anchor = (1, 1))
     ax.set_xlabel("Index")
     ax.set_ylabel(metric_name)
-    ax.figure.savefig("read_tmp.png")
+    ax.figure.set_size_inches(6,3)
+    ax.figure.savefig("read_tmp.png",dpi=dpi,bbox_inches='tight')
 
     #  for write only 
     ax=pdsum_write.plot()
     metric_name="write IOPS"
+    ax.legend(bbox_to_anchor = (1, 1))
     ax.set_xlabel("Index")
     ax.set_ylabel(metric_name)
-    ax.figure.savefig("write_tmp.png")
+    ax.figure.set_size_inches(6,3)
+    ax.figure.savefig("write_tmp.png",dpi=dpi,bbox_inches='tight')
 
     #  for aggregate  
     ax=pdsum_rw.plot()
+    ax.legend(bbox_to_anchor = (1, 1))
     metric_name="read/write IOPS"
     ax.set_xlabel("Index")
     ax.set_ylabel(metric_name)
-    ax.figure.savefig("aggregate_tmp.png")
+    ax.figure.set_size_inches(6,3)
+    ax.figure.savefig("aggregate_tmp.png",dpi=dpi,bbox_inches='tight')
 
     #Special case for iterm2 imgcat
     # subprocess.call(['/bin/bash','-i', '-c','-l', 'imgcat '+imgname])
     #subprocess.call(['/bin/bash','-i', '-c','-l', 'imgcat '+'read_tmp.png'])
-
+    if (args.table):
+        if (not pdsum_read.empty):
+            print("Read Table:")        
+            print(pdsum_read[:5])
+        if (not pdsum_write.empty):
+            print("Write Table:")
+            print(pdsum_write[:5])
+        #Presumably we have something... unless only trims...
+        print("R/W Summary table:")
+        print(pdsum_rw[:5])
 
 if __name__ == "__main__":
     main()
